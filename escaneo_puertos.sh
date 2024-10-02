3c60cfca12c13e7d808716ff1dbd16218cdd88f0
carlos3547 <carlos.ramoszcr@uanl.edu.mx>
2024-10-02 15:34:47 -0600

    Create escaneo_puertos.sh
    Creo el menu el dia 2 de octubre del 2024

---
+++ escaneo_puertos.sh
@@ -0,0 +1,16 @@
+#ruta del reporte
+report="reporte_escaneo.txt"
+
+#menu
+show_menu() {
+  clear
+  echo "escaneo de puertos"
+  echo "1. escaneo de puertos"
+  echo "2. escaneo rapido"
+  echo "3. escaneo detallado"
+  echo "4. escanear puertos abiertos especificos"
+  echo "5. Generar reporte"
+  echo "6. salir
+  read -p "eligue una opcion:" opcion
+
+}
