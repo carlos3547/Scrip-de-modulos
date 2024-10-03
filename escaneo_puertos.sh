71f217717fbd64d85650fa51dbdcbbc4f5429aac
carlos3547 <carlos.ramoszcr@uanl.edu.mx>
2024-10-03 12:51:40 -0600

    Update escaneo_puertos.sh
    Cree la funcion principal el 3 de octubre del 2024
    a las 12:51

--- escaneo_puertos.sh
+++ escaneo_puertos.sh
@@ -12,5 +12,23 @@ show_menu() {
   echo "5. Generar reporte"
   echo "6. salir
   read -p "eligue una opcion:" opcion
-
 }
+# mi funcion principal para el menu
+start_menu() {
+  while true; do
+    mostrar_menu
+    read -p "Elige una opción: " opcion  # Captura la entrada del usuario
+    case $opcion in
+      1) echo "Escaneo de puertos" ;;  
+      2) echo "Escaneo rápido" ;;      
+      3) echo "Escaneo detallado" ;;   
+      4) echo "Escaneo de puertos abiertos" ;;  
+      5) echo "Generar reporte" ;;     
+      6) echo "Saliendo..."; exit ;;   
+      *) echo "Opción no válida" ;;    
+    esac
+    read -p "Presiona Enter para continuar..."  # Pausa antes de mostrar el menú de nuevo
+  done
+}
+
+
